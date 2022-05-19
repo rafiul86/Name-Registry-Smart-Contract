dbf69c083ccedc65658a8b88cf01cfcad5389eb2
RAFIUL HASAN CHOWDHURY <68476971+rafiul86@users.noreply.github.com>
2022-05-19 23:36:06 +0600

    Registration vital functions implemented

--- contracts/VanityName.sol
+++ contracts/VanityName.sol
@@ -1,31 +1,33 @@
 // SPDX-License-Identifier: NONE
 
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.14;
 
 contract VanityNameRegistry {
     uint32 public timeLockPeriod;
-    uint32 public lockValue;
+    uint public lockValue = 1 ether;
     address owner;
     bool locked;
-    constructor(uint32 _timeLockPeriod, uint32 _lockValue){
+    constructor(uint32 _timeLockPeriod){
         timeLockPeriod = _timeLockPeriod;
-        lockValue = _lockValue;
         owner = msg.sender;
         locked = false;
     }
     struct Person {
         string name;
         uint256 value;
+        uint256 time;
         bool isLocked;
     }
     mapping(address => Person) nameBook;
     mapping(string => address) ownerByname;
 
-    function register(string memory _name) public payable {
-        require(msg.value > lockValue, "You need to pay the registration fee");
+    function registerName(string memory _name) public payable {
+        require(msg.value >= lockValue, "You need to pay the registration fee");
         nameBook[msg.sender].name = _name;
         nameBook[msg.sender].value = msg.value;
         nameBook[msg.sender].isLocked = true;
+        nameBook[msg.sender].time = block.timestamp + timeLockPeriod;
+        ownerByname[_name] = msg.sender;
     }
 
     
@@ -38,19 +40,50 @@ contract VanityNameRegistry {
     function setTimeLockPeriod  (uint32 _timeLockPeriod) external  onlyOwner {
         timeLockPeriod = _timeLockPeriod;
     }
-    function setLockValue  (uint32 _lockValue) external  onlyOwner {
+    function setLockValue  (uint _lockValue) external  onlyOwner {
         lockValue = _lockValue;
     }
 
-    function getName(address _owner) public view returns (string memory) {
-        return nameBook[_owner].name;
+    function withdraw () external onlyOwner {
+      require(address(this).balance > 0, "Balance is zero");
+      payable (msg.sender).transfer(address(this).balance);
+    }
+
+    function checkBalance() external view returns (uint) {
+        return address(this).balance;
+    }
+
+    function getName(address _ownerAddress) public view returns (string memory) {
+        return nameBook[_ownerAddress].name;
+    }
+    function getNameCallByOwner() public view returns (string memory) {
+        return nameBook[msg.sender].name;
     }
 
     function getValue(address _owner) public view returns (uint256) {
         return nameBook[_owner].value;
     }
 
+    function removeName(address _removableNameAddress) external {
+        nameBook[_removableNameAddress].name = " ";
+    }
+
+    function withDrawLockValue () external {
+        require(block.timestamp > nameBook[msg.sender].time, "Too early");
+        uint withdrawableBalance = nameBook[msg.sender].value;
+        nameBook[msg.sender].value = 0;
+        payable(msg.sender).transfer(withdrawableBalance);
+    }
+
     function getOwner(string memory _name) public view returns (address) {
         return ownerByname[_name];
+    }
+
+    fallback () external payable {
+
+    }
+
+    receive() external payable {
+        
     }
 }
