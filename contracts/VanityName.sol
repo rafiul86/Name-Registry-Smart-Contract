ccddcc17db0ba0f521e0a8f5ef67bfbdc5a8d698
RAFIUL HASAN CHOWDHURY <68476971+rafiul86@users.noreply.github.com>
2022-05-20 09:49:10 +0600

    String to bytes converted

--- contracts/VanityName.sol
+++ contracts/VanityName.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 
 contract VanityName {
     uint32 public timeLockPeriod;
-    uint public lockValue = 1 ether;
+    uint public lockValue = 0.0001 ether;
     address owner;
     bool locked;
     constructor(uint32 _timeLockPeriod){
@@ -13,7 +13,7 @@ contract VanityName {
         locked = false;
     }
     struct Person {
-        bytes32 name;
+        bytes name;
         uint256 value;
         uint256 time;
         bool isLocked;
@@ -21,22 +21,35 @@ contract VanityName {
     mapping(address => Person) nameBook;
     mapping(string => address) ownerByname;
 
-    function registerName(bytes32 _name) public payable {
+    function registerName(string memory _name) public payable {
         require(msg.value >= lockValue, "You need to pay the registration fee");
+       bytes memory nameFromString = bytes(_name);
         if (msg.value > lockValue){
             uint extraValue = msg.value - lockValue;
             payable(msg.sender).transfer(extraValue);
             uint remainingValue = msg.value - extraValue;
-            nameBook[msg.sender].name = _name;
+            nameBook[msg.sender].name = nameFromString;
             nameBook[msg.sender].value = remainingValue;
             nameBook[msg.sender].isLocked = true;
             nameBook[msg.sender].time = block.timestamp + timeLockPeriod;
         } else {
-            nameBook[msg.sender].name = _name;
+            nameBook[msg.sender].name = nameFromString;
             nameBook[msg.sender].value = msg.value;
             nameBook[msg.sender].isLocked = true;
             nameBook[msg.sender].time = block.timestamp + timeLockPeriod;
         }        
+    }
+
+    function bytes32ToString(bytes32 _bytes32) public pure returns (string memory) {
+        uint8 i = 0;
+        while(i < 32 && _bytes32[i] != 0) {
+            i++;
+        }
+        bytes memory bytesArray = new bytes(i);
+        for (i = 0; i < 32 && _bytes32[i] != 0; i++) {
+            bytesArray[i] = _bytes32[i];
+        }
+        return string(bytesArray);
     }
 
     modifier onlyOwner {
@@ -60,11 +73,13 @@ contract VanityName {
         return address(this).balance;
     }
 
-    function getName(address _ownerAddress) public view returns (bytes32) {
-        return nameBook[_ownerAddress].name;
+    function getName(address _ownerAddress) public view returns (string memory) {
+        string memory nameFromBytes = string(nameBook[_ownerAddress].name);
+        return nameFromBytes;
     }
-    function getNameCallByOwner() public view returns (bytes32) {
-        return nameBook[msg.sender].name;
+    function getNameCallByOwner() public view returns (string memory) {
+        string memory nameFromBytes = string(nameBook[msg.sender].name);
+        return nameFromBytes;
     }
 
     function getValue(address _owner) public view returns (uint256) {
